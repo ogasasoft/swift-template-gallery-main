997b07676881533376b0100b71b7c89a27bfdd7c
aiamadeuss <aiamadeuss@gmail.com>
2026-06-21 03:10:07 +0900

    fix: remove unused imports from cypress.config.ts

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,18 +1,9 @@
 import { defineConfig } from 'cypress';
-import { definePerTestTimeouts } from '@cypress/code-coverage/dist/common';
-import { getVideoOptions } from 'cypress-mochawesome-reporter/dist/config';
-import { definePerTestVideoOptions } from '@cypress/code-coverage/dist/common';
 
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
-      definePerTestTimeouts(config, {
-        default: 10000,
-        command: 5000,
-        pageLoad: 30000,
-        request: 5000,
-      });
     },
     specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
     baseUrl: 'http://localhost:5173',
